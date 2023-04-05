643c58f334a5c8440a8093d616f0e026eb07de67
Alexander Kovalchuk <beckham3@yandex.ru>
2023-04-05 16:51:58 +0300

    add src

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("DZ miles");
+        int ticketCost = 2500;
+        int rurForOneMile = 20;
+        int bonus = ticketCost / rurForOneMile;
+        System.out.println(bonus);
+    }
+
+}
